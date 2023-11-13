40033ee43ed9e333619ba68b1566f8c74af65544
Konner Horton <konnerhorton@gmail.com>
2023-11-13 14:30:16 -0500

    Update

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -2,31 +2,31 @@ require("dotenv").config();
 
 module.exports = {
     // Website title, shown in left sidebar and in page title
-    title: "{ rené.winkelmeyer }",
+    title: "Konner Horton",
     // Site URL to generate absolute URLs. Used across the board.
     url: process.env.URL || "http://localhost:8080",
     // Profile image for left sidebar
     image: "/assets/images/sidebar_profile.jpg",
     // Image alt text for left sidebar
-    imageAlt: "René and Einstein",
+    imageAlt: "Konner Horton",
     // Author name, shown in left sidebar, and used in JSON-LD
-    author: "René Winkelmeyer",
+    author: "Konner Horton",
     // Site description, shown below site image (optional)
-    description: "My kitchen sink about stuff",
+    description: "A resume",
     // OpenGraph default image, in case you don't have an `image`
     // set in your Markdown frontmatter; relevant for social
     // sharing.
     openGraphDefaultImage: "/assets/images/opengraph.jpg",
     // GitHub ID (optional, remove it not needed), used for link in the left sidebar
-    socialGitHub: "muenzpraeger",
+    socialGitHub: "konnerhorton",
     // LinkedIn ID  (optional, remove it not needed), used for link in the left sidebar
-    socialLinkedIn: "muenzpraeger",
+    // socialLinkedIn: "konnerhorton",
     // Twitter ID  (optional, remove it not needed), used for link in the left sidebar, and for OpenGraph sharing information
-    socialTwitter: "muenzpraeger",
+    // socialTwitter: "konnerhorton",
     // YouTube ID/Channel  (optional, remove it not needed), used for link in the left sidebar
-    socialYouTube: "UCH60RRaY2GI9m62z1loLjcA",
+    // socialYouTube: "UCH60RRaY2GI9m62z1loLjcA",
     // Google Analytics ID  (optional, remove it not needed), used for... well, Google Analytics
-    googleAnalytics: "YOUR_GA-ID",
+    // googleAnalytics: "YOUR_GA-ID",
     // Algolia-powered search  (optional, remove it not needed),
     // See: https://github.com/algolia/algoliasearch-netlify
     algoliaSearch: {
